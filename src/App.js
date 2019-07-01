3a6bc715b70c7f9464ba0e51e6a7c5dc207b5bbe
Snowy <1309571292@qq.com>
2019-07-01 11:39:51 +0800

    change paragraph

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 import React from 'react';
+import ListContacts from './ListContacts'
+import './index.css'
 
 function App() {
+  const contacts = [
+    {
+      "id": "ryan",
+      "name": "Ryan Florence",
+      "email": "ryan@reacttraining.com",
+      "avatarURL": "http://localhost:5001/ryan.jpg"
+    },
+    {
+      "id": "michael",
+      "name": "Michael Jackson",
+      "email": "michael@reacttraining.com",
+      "avatarURL": "http://localhost:5001/michael.jpg"
+    },
+    {
+      "id": "tyler",
+      "name": "Tyler McGinnis",
+      "email": "tyler@reacttraining.com",
+      "avatarURL": "http://localhost:5001/tyler.jpg"
+    }
+  ]
   return (
-    <div className="App">contacts
+    <div className="App">
+      <ListContacts contacts = {contacts}></ListContacts>
     </div>
   );
 }
